9f1e36d388587b11dcf38a51bdfd5b36148d0bb3
Mosh Feu <moshfeu@gmail.com>
2020-08-29 22:32:31 +0300

    fix:icons-broken (#20)

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import { init } from './commands';
 import { ExtensionContext } from 'vscode';
+import { log } from './logger';
 
 export function activate(context: ExtensionContext) {
   init(context);
+  log(process.env);
 }
