c6a79ab1478a8203964c7335efe2d0038a50fe50
Moshe Feuchtwanger <moshfeu@gmail.com>
2022-03-11 00:49:39 +0200

    remove global types for now

--- resources/monaco/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="node_modules/monaco-editor/monaco.d.ts" />
-
-interface Window {
-  diffNavigator: monaco.editor.IDiffNavigator;
-  diffEditor: monaco.editor.IStandaloneDiffEditor;
-}
